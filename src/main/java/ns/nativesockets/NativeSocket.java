f2b5b7e65d941b8824a066d95ec3579329753b40
Nuke Sparrow <nukesparrow@bitmessage.ch>
2016-05-20 17:54:51 +0300

    unused import

--- src/main/java/ns/nativesockets/NativeSocket.java
+++ src/main/java/ns/nativesockets/NativeSocket.java
@@ -27,7 +27,6 @@ import java.lang.reflect.Method;
 import java.net.Inet6Address;
 import java.net.InetSocketAddress;
 import java.util.Arrays;
-import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
